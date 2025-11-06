7c91d30337fb07fbfc3fa27bf5b77174ba5f2081
Kastov Rostislav <rkastov@gmail.com>
2025-11-06 23:30:40 +0300

    в моем понимании вроде как все сделано

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import './scss/styles.scss';
+import { IProduct } from './types';
+import { apiProducts } from './utils/data';
+
+class Product {
+  item: IProduct[];
+
+  constructor() {
+    this.item = [];
+  }
+
+  setItems(item: IProduct[]):void {
+    this.item = item
+  }
+  getItems(): IProduct[] {
+    return this.item
+  }
+}
+
+const productsModel = new Product();
+productsModel.setItems(apiProducts.items);
+
+console.log(`Массив товаров из каталога:`, productsModel.getItems())
